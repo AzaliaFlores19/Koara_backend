ce731c7d739d15683576045fcd686a09302243f8
AzaliaFlores19 <yamiflo19@unitec.edu>
2026-05-20 11:36:37 -0600

    Added CAI RANGE  controller with admin guards

--- src/cai_range/cai_range.controller.ts
+++ src/cai_range/cai_range.controller.ts
@@ -1,3 +1,39 @@
+import { Controller, Get, Post, Put, Patch, Body, Param, ParseUUIDPipe } from '@nestjs/common';
+import { CaiService } from './cai.service';
+import { CreateCaiDto } from './create-cai-dto';
+import { UpdateCaiDto } from './update-cai-dto';
+@Controller('cai')
+export class CaiController {
+  constructor(private readonly caiService: CaiService) {}
+
+  @Post()
+  create(@Body() createCaiDto: CreateCaiDto) {
+    return this.caiService.create(createCaiDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.caiService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.caiService.findById(id);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateCaiDto: UpdateCaiDto) {
+    return this.caiService.updateCai(id, updateCaiDto);
+  }
+
+  @Patch(':id/deactivate')
+  deactivate(@Param('id', ParseUUIDPipe) id: string) {
+    return this.caiService.deactivateCai(id);
+  }
+}
+///////////////////////////////////////////////////////////////////////////
+
+
 import { Controller, Get, Post, Patch, Body, Param, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { CaiRangeService } from './cai_range.service';
 import { CreateCaiRangeDto } from './create-cai-range.dto';
@@ -11,5 +47,42 @@ import { Roles } from '../auth/decorators/roles.decorator';
 export class CaiRangeController {
   constructor(private readonly caiRangeService: CaiRangeService) {}
 
+  @Post()
+  @Roles('ADMIN')
+  create(@Body() createCaiRangeDto: CreateCaiRangeDto) {
+    return this.caiRangeService.createCaiRange(createCaiRangeDto);
+  }
 
+  @Get()
+  @Roles('ADMIN')
+  findAll() {
+    return this.caiRangeService.findAll();
+  }
+
+  @Get('active')
+  @Roles('ADMIN')
+  findActive() {
+    return this.caiRangeService.findActive();
+  }
+
+  @Get(':id')
+  @Roles('ADMIN')
+  findById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.caiRangeService.findById(id);
+  }
+
+  @Patch(':id')
+  @Roles('ADMIN')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCaiRangeDto: UpdateCaiRangeDto,
+  ) {
+    return this.caiRangeService.updateCaiRange(id, updateCaiRangeDto);
+  }
+
+  @Patch(':id/deactivate')
+  @Roles('ADMIN')
+  deactivate(@Param('id', ParseUUIDPipe) id: string) {
+    return this.caiRangeService.deactivateCaiRange(id);
+  }
 }
