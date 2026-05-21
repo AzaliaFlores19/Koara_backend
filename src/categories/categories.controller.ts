869577ba9a0713a89325d96da71538001824fadb
DanDavD <124285697+DanDavD@users.noreply.github.com>
2026-05-20 22:43:30 -0600

    full crud categorias parte 2

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Get,
   Post,
+  Put,
   Patch,
   Body,
   Param,
@@ -14,26 +15,39 @@ import { UpdateCategoryDto } from './update-category.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 
-@UseGuards(JwtAuthGuard)
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() dto: CreateCategoryDto, @CurrentUser('id') userId: string) {
     return this.categoriesService.create(dto, userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(@Query('page') page?: string, @Query('limit') limit?: string) {
     return this.categoriesService.findAll(page ? +page : 1, limit ? +limit : 10);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Put('/:id')
+  replace(
+    @Param('id') id: string,
+    @Body() dto: CreateCategoryDto,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.categoriesService.update(id, dto, userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Patch('/:id')
   update(
     @Param('id') id: string,
@@ -43,6 +57,7 @@ export class CategoriesController {
     return this.categoriesService.update(id, dto, userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch('/:id/deactivate')
   deactivate(@Param('id') id: string, @CurrentUser('id') userId: string) {
     return this.categoriesService.deactivate(id, userId);
